18664690c06e14c4d5741e3febc6a129c76c663b
Giulia Maino <giuliamaino97@gmail.com>
2024-08-30 17:00:51 +0200

    added exercises

--- D10.js
+++ D10.js
@@ -67,9 +67,25 @@ console.log(me);
   Crea una funzione chiamata "dice": deve generare un numero casuale tra 1 e 6.
 */
 
+function dice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+console.log("You rolled a " + dice());
+
 /* ESERCIZIO 2
   Crea una funzione chiamata "whoIsBigger" che riceve due numeri come parametri e ritorna il maggiore dei due.
 */
+
+function whoIsBigger(num1, num2) {
+  if (num1 < num2) {
+    return console.log("The bigger number is " + num2);
+  } else if (num1 > num2) {
+    return console.log("The bigger number is " + num1);
+  } else {
+    console.log("The numbers are the same");
+  }
+}
+whoIsBigger(11, 6);
 
 /* ESERCIZIO 3
   Crea una funzione chiamata "splitMe" che riceve una stringa come parametro e ritorna un'array contenente ogni parola della stringa.
@@ -77,10 +93,32 @@ console.log(me);
   Es.: splitMe("I love coding") => ritorna ["I", "Love", "Coding"]
 */
 
+function splitMe(str) {
+  let singleWords = str.split(" ");
+  let upperCaseString = [];
+  for (let i = 0; i < singleWords.length; i++) {
+    let firstChar = singleWords[i].charAt(0);
+    let upperCaseChar = firstChar.toUpperCase();
+    let leftChar = singleWords[i].slice(1);
+    let joinedChar = upperCaseChar + leftChar;
+    upperCaseString.push(joinedChar);
+  }
+  console.log(upperCaseString.join(" "));
+}
+splitMe("The last movie I watched is The Royal Tenenbaums");
+
 /* ESERCIZIO 4
   Crea una funzione chiamata "deleteOne" che riceve una stringa e un booleano come parametri.
   Se il valore booleano è true la funzione deve ritornare la stringa senza il primo carattere, altrimenti la deve ritornare senza l'ultimo.
 */
+
+function deleteOne(str, par2) {
+  if (par2) {
+    return console.log(str.slice(1));
+  }
+  return console.log(str.slice(0, str.length - 1));
+}
+deleteOne("The last movie I watched is The Royal Tenenbaums", "anything");
 
 /* ESERCIZIO 5
   Crea una funzione chiamata "onlyLetters" che riceve una stringa come parametro e la ritorna eliminando tutte le cifre numeriche.
@@ -88,13 +126,40 @@ console.log(me);
   Es.: onlyLetters("I have 4 dogs") => ritorna "I have dogs"
 */
 
+function onlyLetters(str) {
+  const characters = str.split("");
+  const deleteNum = characters.filter((char) => !Number(char));
+  return console.log(deleteNum.join(""));
+}
+
+onlyLetters("On my desk I have 2 screens");
+
 /* ESERCIZIO 6
   Crea una funzione chiamata "isThisAnEmail" che riceve una stringa come parametro e ritorna true se la stringa è un valido indirizzo email.
 */
 
+function isThisAnEmail(str) {
+  const at = /@/;
+  const fullStop = /./;
+  if (str.match(at) && str.match(fullStop)) {
+    return true;
+  }
+  return false;
+}
+console.log(isThisAnEmail("giuliamaino97@gmail.com"));
+
 /* ESERCIZIO 7
   Scrivi una funzione chiamata "whatDayIsIt" che ritorna il giorno della settimana corrente.
 */
+
+const now = new Date();
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function whatDayIsIt() {
+  const dayIndex = now.getDay();
+  const today = dayNames[dayIndex];
+  return console.log(today);
+}
+whatDayIsIt();
 
 /* ESERCIZIO 8
   Scrivi una funzione chiamata "rollTheDices" che riceve un numero come parametro.
@@ -109,13 +174,57 @@ console.log(me);
   }
 */
 
+console.log(dice());
+
+function rollTheDice(number) {
+  let sum = 0;
+  let values = [];
+
+  for (i = 0; i < number; i++) {
+    const x = dice();
+    sum = sum + x;
+    values.push(x);
+  }
+  return {
+    sum: sum,
+    values: values,
+  };
+}
+console.log(rollTheDice(5));
+
 /* ESERCIZIO 9
   Scrivi una funzione chiamata "howManyDays" che riceve una data come parametro e ritorna il numero di giorni trascorsi da tale data.
 */
 
+function howManyDays(date) {
+  const today = new Date();
+  const otherDate = new Date(date);
+  let difference;
+  if (today < otherDate) {
+    difference = new Date(otherDate - today);
+  } else if (today > otherDate) {
+    difference = new Date(today - otherDate);
+  } else {
+    return 0;
+  }
+  differenceInDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+  return differenceInDays;
+}
+console.log(howManyDays("2023-08-30"));
+
 /* ESERCIZIO 10
   Scrivi una funzione chiamata "isTodayMyBirthday" che deve ritornare true se oggi è il tuo compleanno, falso negli altri casi.
 */
+
+function isTodayMyBirthday() {
+  const today = new Date();
+  if (today.getDate() === 26 && today.getMonth() === 2) {
+    console.log("Oggi è il mio compleanno!");
+  } else {
+    console.log("Oggi non è il mio compleanno :(");
+  }
+}
+isTodayMyBirthday();
 
 // Arrays & Oggetti
 
@@ -126,29 +235,78 @@ console.log(me);
   in esso la proprietà chiamata come la stringa passata come secondo parametro.
 */
 
+function deleteProp(obj, str) {
+  if (obj.hasOwnProperty(str)) {
+    delete obj[str];
+  }
+  return obj;
+}
+console.log(deleteProp({ name: "Frodo", surname: "Baggins", age: "33", hair: "brown", eyes: "blue" }, "surname"));
+
 /* ESERCIZIO 12
   Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
 */
+
+function newestMovie(moviesArray) {
+  let movieObj = { Year: 1963 };
+  moviesArray.forEach((movie) => {
+    let movieYear = parseInt(movie.Year);
+    if (movieYear > movieObj.Year) {
+      movieObj = movie;
+    }
+  });
+
+  return movieObj;
+}
 
 /* ESERCIZIO 13
   Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
 */
 
+const countMovies = function (moviesArray) {
+  return moviesArray.length;
+};
+
 /* ESERCIZIO 14
   Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
 */
+
+const onlyTheYears = function (moviesArray) {
+  return moviesArray.map((movie) => movie.Year);
+};
 
 /* ESERCIZIO 15
   Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
 */
 
+const onlyInLastMillennium = function (moviesArray) {
+  return moviesArray.filter((movie) => {
+    return parseInt(movie.Year) < 2000;
+  });
+};
+
 /* ESERCIZIO 16
   Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
 */
 
+const sumAllTheYears = function (moviesArray) {
+  return moviesArray.reduce((accumulator, current) => accumulator + parseInt(current.Year), 0);
+};
+
 /* ESERCIZIO 17
   Scrivi una funzione chiamata "searchByTitle" che riceve una stringa come parametro e ritorna i film nell'array "movies" fornito che la contengono nel titolo.
 */
+
+function searchByTitle(str) {
+  const filteredMovies = movies
+    .map((movie) => {
+      if (movie.Title.includes(str)) {
+        return movie;
+      }
+    })
+    .filter((movie) => movie !== undefined);
+  return filteredMovies;
+}
 
 /* ESERCIZIO 18
   Scrivi una funzione chiamata "searchAndDivide" che riceve una stringa come parametro e ritorna un oggetto contenente due array: "match" e "unmatch".
@@ -165,29 +323,71 @@ console.log(me);
   Scrivi una funzione per selezionare l'elemento dotato di id "container" all'interno della pagina.
 */
 
+function getContainer() {
+  const container = document.getElementById("container");
+  return container;
+}
 /* ESERCIZIO 21
   Scrivi una funzione per selezionare ogni tag <td> all'interno della pagina.
 */
+
+function getTd() {
+  const td = document.getElementsByTagName("td");
+  return td;
+}
 
 /* ESERCIZIO 22
   Scrivi una funzione che, tramite un ciclo, stampa in console il testo contenuto in ogni tag <td> all'interno della pagina.
 */
 
+function printTd() {
+  const tableData = document.querySelectorAll("td");
+
+  for (let i = 0; i < tableData.length; i++) {
+    console.log(tableData[i].innerText);
+  }
+}
+
 /* ESERCIZIO 23
   Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
 */
+
+function redBackground() {
+  const link = document.querySelectorAll("a");
+
+  for (let i = 0; i < link.length; i++) {
+    link[i].style.backgroundColor = "red";
+  }
+}
 
 /* ESERCIZIO 24
   Scrivi una funzione per aggiungere un nuovo elemento alla lista non ordinata con id "myList".
 */
 
+function addListItem(text) {
+  const myList = document.getElementById("myList");
+  const newListItem = document.createElement("li");
+  myList.appendChild(newListItem);
+}
+
 /* ESERCIZIO 25
   Scrivi una funzione per svuotare la lista non ordinata con id "myList".
 */
+function emptyList() {
+  const myList = document.getElementById("myList");
+  myList.innerHTML = "";
+}
 
 /* ESERCIZIO 26
   Scrivi una funzione per aggiungere ad ogni tag <tr> la classe CSS "test"
 */
+
+function addClass() {
+  const tr = document.querySelectorAll("tr");
+  tr.forEach((row) => {
+    row.classList.add("test");
+  });
+}
 
 // [EXTRA] JS Avanzato
 
@@ -221,104 +421,122 @@ console.log(me);
 
 /* Questo array viene usato per gli esercizi. Non modificarlo. */
 
-// const movies = [
-//   {
-//     Title: "The Lord of the Rings: The Fellowship of the Ring",
-//     Year: "2001",
-//     imdbID: "tt0120737",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-//   },
+const movies = [
+  {
+    Title: "The Lord of the Rings: The Fellowship of the Ring",
+    Year: "2001",
+    imdbID: "tt0120737",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+  },
 
-//   {
-//     Title: "The Lord of the Rings: The Return of the King",
-//     Year: "2003",
-//     imdbID: "tt0167260",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "The Lord of the Rings: The Two Towers",
-//     Year: "2002",
-//     imdbID: "tt0167261",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "Lord of War",
-//     Year: "2005",
-//     imdbID: "tt0399295",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "Lords of Dogtown",
-//     Year: "2005",
-//     imdbID: "tt0355702",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "The Lord of the Rings",
-//     Year: "1978",
-//     imdbID: "tt0077869",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "Lord of the Flies",
-//     Year: "1990",
-//     imdbID: "tt0100054",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "The Lords of Salem",
-//     Year: "2012",
-//     imdbID: "tt1731697",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
-//     Year: "1984",
-//     imdbID: "tt0087365",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "Lord of the Flies",
-//     Year: "1963",
-//     imdbID: "tt0057261",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "The Avengers",
-//     Year: "2012",
-//     imdbID: "tt0848228",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "Avengers: Infinity War",
-//     Year: "2018",
-//     imdbID: "tt4154756",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "Avengers: Age of Ultron",
-//     Year: "2015",
-//     imdbID: "tt2395427",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
-//   },
-//   {
-//     Title: "Avengers: Endgame",
-//     Year: "2019",
-//     imdbID: "tt4154796",
-//     Type: "movie",
-//     Poster: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
-//   },
-// ];
+  {
+    Title: "The Lord of the Rings: The Return of the King",
+    Year: "2003",
+    imdbID: "tt0167260",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lord of the Rings: The Two Towers",
+    Year: "2002",
+    imdbID: "tt0167261",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lord of War",
+    Year: "2005",
+    imdbID: "tt0399295",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lords of Dogtown",
+    Year: "2005",
+    imdbID: "tt0355702",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lord of the Rings",
+    Year: "1978",
+    imdbID: "tt0077869",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lord of the Flies",
+    Year: "1990",
+    imdbID: "tt0100054",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lords of Salem",
+    Year: "2012",
+    imdbID: "tt1731697",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
+  },
+  {
+    Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+    Year: "1984",
+    imdbID: "tt0087365",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lord of the Flies",
+    Year: "1963",
+    imdbID: "tt0057261",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Avengers",
+    Year: "2012",
+    imdbID: "tt0848228",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+  },
+  {
+    Title: "Avengers: Infinity War",
+    Year: "2018",
+    imdbID: "tt4154756",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+  },
+  {
+    Title: "Avengers: Age of Ultron",
+    Year: "2015",
+    imdbID: "tt2395427",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+  },
+  {
+    Title: "Avengers: Endgame",
+    Year: "2019",
+    imdbID: "tt4154796",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+  },
+];
+
+//ex 12
+console.log(newestMovie(movies));
+
+//ex 13
+console.log(countMovies(movies));
+
+//ex 14
+console.log(onlyTheYears(movies));
+
+//ex 15
+console.log(onlyInLastMillennium(movies));
+
+//ex 16
+console.log(sumAllTheYears(movies));
+
+//ex 17
+console.log(searchByTitle("The Lord of the Rings"));
